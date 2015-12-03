f12fbf8153d373d68ec583f2b65cc7a1c27b2850
kpdecker <kpdecker@gmail.com>
2015-12-02 17:49:42 -0800

    Remove non-electron components

--- tasks/webpack.js
+++ tasks/webpack.js
@@ -1,27 +1,10 @@
 import Gulp from 'gulp';
 import GUtil from 'gulp-util';
 import webpack from 'webpack';
-import WebpackDevServer from 'webpack-dev-server';
 
-import {CLIENT_ENTRY, BUILD_TARGET, SERVER_PORT, DEV_SERVER_PORT, WATCHING} from '../index';
+import {BUILD_TARGET, WATCHING} from 'linoleum/index';
 import loadWebpackConfig from '../src/webpack';
 
-Gulp.task('webpack', function(done) {
-  let web = loadWebpackConfig(),
-      server = loadWebpackConfig({
-        node: true,
-        entry: {index: './src/index'},
-        path: `${BUILD_TARGET}/`
-      }),
-      cover = loadWebpackConfig({
-        node: true,
-        cover: true,
-        entry: {cover: require.resolve('../src/webpack-server-test')},
-        path: `${BUILD_TARGET}/$cover$/`
-      });
-
-  webpack([web, server, cover], handleWebpack(done));
-});
 Gulp.task('webpack:electron', function(done) {
   let main = loadWebpackConfig({
         electron: true,
@@ -38,13 +21,13 @@ Gulp.task('webpack:electron', function(done) {
         electron: true,
         node: true,
         cover: true,
-        entry: {main: require.resolve('../src/webpack-server-test')},
+        entry: {main: require.resolve('linoleum-webpack/src/webpack-server-test')},
         path: `${BUILD_TARGET}/$cover$/`
       }),
       coverRenderer = loadWebpackConfig({
         electron: true,
         cover: true,
-        entry: {renderer: require.resolve('../src/webpack-web-test')},
+        entry: {renderer: require.resolve('linoleum-webpack/src/webpack-web-test')},
         path: `${BUILD_TARGET}/$cover$/`
       });
 
@@ -62,68 +45,3 @@ function handleWebpack(done) {
     done();
   };
 }
-
-Gulp.task('webpack:dev-server', function(done) {
-  let devServer = `http://localhost:${DEV_SERVER_PORT}/`,
-      config = loadWebpackConfig({
-        entry: {
-          bootstrap: [
-            `${require.resolve('webpack-dev-server/client')}?${devServer}`,
-            require.resolve(`webpack/hot/only-dev-server`),
-            CLIENT_ENTRY
-          ]
-        }
-      });
-  config.plugins.unshift(
-    new webpack.HotModuleReplacementPlugin()
-  );
-
-  config.output.path = '/static/';
-  config.output.publicPath = `${devServer}static/`;
-
-  config.module.loaders.forEach((loader) => {
-    if (loader.babel) {
-      loader.query.plugins = loader.query.plugins.concat(
-        require.resolve('babel-plugin-react-transform'));
-      loader.query.extra = {
-        'react-transform': {
-          transforms: [{
-            transform: require.resolve('react-transform-hmr'),
-            imports: ['react'],
-            locals: ['module']
-          }]
-        }
-      };
-    }
-  });
-
-  let server = new WebpackDevServer(webpack(config), {
-    // webpack-dev-server options
-    contentBase: 'http://localhost:3000/',
-
-    hot: true,
-    proxy: {
-      '*': `http://localhost:${SERVER_PORT}`
-    },
-
-    // webpack-dev-middleware options
-    // NOTE: We can not use lazy here as this conflicts with the proxy
-    lazy: false,
-    quiet: false,
-    noInfo: false,
-    watchOptions: {
-      aggregateTimeout: 300,
-      poll: 1000
-    },
-    publicPath: '/static/',
-    stats: {
-      colors: true,
-      chunks: false,
-      version: false
-    }
-  });
-  server.listen(DEV_SERVER_PORT, 'localhost', function() {
-    console.log(`Dev server listening on ${devServer}`);
-    done();
-  });
-});
--- test/webpack.server.js
+++ test/webpack.server.js
@@ -1,50 +1,14 @@
-/* eslint-disable no-process-env */
-import webpack from '../src/webpack';
+import webpackConfig from '../src/webpack';
 
 import {expect} from 'chai';
 
 describe('webpack config', function() {
-  let env;
-  beforeEach(() => {
-    env = process.env.NODE_ENV;
+  it('should provide electron specifics', function() {
+    let config = webpackConfig();
+    expect(config.devtool).to.equal('source-map');
   });
-  afterEach(() => {
-    process.env.NODE_ENV = env;
-  });
-
-  it('should generate production config', function() {
-    process.env.NODE_ENV = 'production';
-
-    let config = webpack();
-    expect(config.plugins).to.have.length(3);
-  });
-
-  it('should generate node config', function() {
-    let config = webpack({node: true});
-    expect(config.target).to.equal('node');
-  });
-
-  it('should generate electron config', function() {
-    let config = webpack({electron: true});
-    expect(config.target).to.equal('electron');
-  });
-
-  it('should export proper externals', function(done) {
-    let config = webpack({node: true});
-    config.externals[0](undefined, 'foo', (err, request, type) => {
-      expect(err).to.not.exist;
-      expect(request).to.equal('foo');
-      expect(type).to.equal('commonjs2');
-      done();
-    });
-  });
-  it('should not export internals', function(done) {
-    let config = webpack({node: true});
-    config.externals[0](undefined, './foo', (err, request, type) => {
-      expect(err).to.not.exist;
-      expect(request).to.not.exist;
-      expect(type).to.not.exist;
-      done();
-    });
+  it('should handle custom config', function() {
+    let config = webpackConfig({});
+    expect(config.devtool).to.equal('source-map');
   });
 });
